3790447e924ed103a1921933114a53a2a1745f82
gokulsidd <gokulsidharth02@gmail.com>
2025-12-02 18:38:09 +0530

    initial commit

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    output: "standalone",
+    basePath: "/DocumentWorkspaceV2",  // ✅ Ensure this matches IIS site path
+    assetPrefix: "/DocumentWorkspaceV2/", // ✅ Ensures correct static file paths
+    trailingSlash: true, // ✅ Helps with IIS routing
+
+
+    turbopack: {},
+
+    async rewrites() {
+        return [
+            {
+                source: "/_next/:path*",
+                destination: "/DocumentWorkspaceV2/_next/:path*", // ✅ Fix for static assets
+            },
+        ];
+    },
+};
+
+export default nextConfig;
